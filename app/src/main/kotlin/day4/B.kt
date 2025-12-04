2e1accb536d4f228796b75797921dc3a35f88603
Pibo Shongwe <piboman@yahoo.com>
2025-12-04 08:36:08 +0200

    day 4

--- app/src/main/kotlin/day4/B.kt
+++ app/src/main/kotlin/day4/B.kt
@@ -13,26 +13,4 @@ fun main(args: Array<String>) {
 
     // Solve Problem
 
-    // Make a copy to write the result into to avoid errors
-    val newGrid = Array(grid.size) { r -> grid[r].clone() }
-
-    var count = 0
-    grid.forEachCell { r, c, value ->
-        val hasFourOrMore = grid.hasAtLeastNNeighborsOf(r, c, '@', 4)
-        if (value == '@' && !hasFourOrMore) {
-            newGrid[r][c] = 'x'
-            count++
-        }
-    }
-
-    // Copy result back for final print
-    for (r in grid.indices) {
-        grid[r] = newGrid[r]
-    }
-
-    println("Result Grid: ")
-    println("----------")
-    grid.forEach { println(it.concatToString()) }
-    println("----------")
-    println("Total rolls: $count")
 }
