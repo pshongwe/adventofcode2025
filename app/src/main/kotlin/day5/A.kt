a1f48bf1a00550a8a4b76266f9c006bc2048312e
Pibo Shongwe <piboman@yahoo.com>
2025-12-05 07:01:45 +0200

    day 5

---
+++ app/src/main/kotlin/day5/A.kt
@@ -0,0 +1,13 @@
+package day5
+
+import utils.InputReader
+
+fun main(args: Array<String>) {
+    // Prep Data:
+    val lines = InputReader.readLinesForDay(4, args)
+
+
+    // Solve Problem
+
+
+}
