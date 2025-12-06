1844bc530de55836bb95ac6082dc2f72c9f4acf8
Pibo Shongwe <piboman@yahoo.com>
2025-12-06 07:56:06 +0200

    day 6

--- app/src/main/kotlin/day6/B.kt
+++ app/src/main/kotlin/day6/B.kt
@@ -4,7 +4,7 @@ import utils.InputReader
 
 fun main(args: Array<String>) {
     // Prep Data:
-    val lines = InputReader.readLinesForDay(5, args)
+    val lines = InputReader.readLinesForDay(6, args)
 
 
     // Solve Problem
